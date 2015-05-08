7134cb6a7152cf9c39f18eb555f3e95f4429c867
Sai Tseng <s101321004@mail1.ncnu.edu.tw>
2015-05-08 12:48:36 +0800

    ver0.4
    1.<img>新增url輸入(o)
    2.text排版(x)
    3.top-bar(x)

--- create.php
+++ create.php
@@ -2,11 +2,15 @@
 	header("Content-Type:text/html; charset=utf-8");
 
 	$fname = $_POST["filename"];
-	echo $fname.".html";
 
-	$filename = iconv('UTF-8','Big5//IGNORE',trim($fname));
-	$str = "Hello World";
-	$file = fopen("Sub/".$filename.".html","a+"); //開啟檔案
+	$str = $_POST["content"];
+	$str = str_replace('\\"', '"', $str);
+	echo $fname.".html";
+	echo "<br>".$str;
+
+	$filename = iconv('UTF-8','Big5//IGNORE',trim($fname));//因為window檔名預存格式是Big5
+
+	$file = fopen("Sub/".$filename.".html","w"); //開啟檔案  
 
 	fwrite($file,$str);
 
